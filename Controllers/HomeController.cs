b6779091d43138100ca6fa6a4e8186bee3dfa405
Rohan Singh <rsin0021@student.monash.edu>
2020-04-23 11:29:59 +1000

    files upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ie_sport_injury_ver_1._3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your education page.";
+
+            return View();
+        }
+
+        public ActionResult Quiz()
+        {
+            ViewBag.Message = "Your quiz page.";
+
+            return View();
+        }
+
+        public ActionResult WarmUp()
+        {
+            ViewBag.Message = "Your WarmUp page.";
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Your Map page.";
+
+            return View();
+        }
+
+    }
+}
